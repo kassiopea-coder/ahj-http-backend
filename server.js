7d3777250f81154f63cfc506a8f792894f8e61f7
Nadezhda <7_nadezhda@mail.ru>
2021-11-21 18:33:35 +0400

    fix server

--- server.js
+++ server.js
@@ -1,129 +1,70 @@
 const http = require('http');
 const Koa = require('koa');
 const koaBody = require('koa-body');
+const Router = require('koa-router');
+const cors = require('koa2-cors');
+const Tickets = require('./Tickets');
+
 const app = new Koa();
 
-class Ticket {
-  constructor(id, name, status, created) {
-    this.id = id // идентификатор (уникальный в пределах системы)
-    this.name = name // краткое описание
-    this.status = status // boolean - сделано или нет
-    this.created = created // дата создания (timestamp)
-  }
-}
-
-class TicketFull {
-  constructor(id, name, description, status, created) {
-    this.id = id // идентификатор (уникальный в пределах системы)
-    this.name = name // краткое описание
-    this.description = description // полное описание
-    this.status = status // boolean - сделано или нет
-    this.created = created // дата создания (timestamp)
-  }
-}
-
-let ticketsFull = [
-  new TicketFull(0, 'Install Win', 'Install Windows 10, drivers for printer, MS Office, save documents and mediafiles', false, new Date().toString().slice(3,21)),
-  new TicketFull(1, 'Replace cartridge', 'Replace cartridge for printer Samsung in cabinet #404', true, new Date().toString().slice(3,21))
-];
+// Body Parsers
+app.use(koaBody({ json: true, text: true, urlencoded: true }));
 
 // CORS
-app.use(async (ctx, next) => {
-  const origin = ctx.request.get('Origin');
-  if (!origin) {
-    return await next();
-  }
-  
-  const headers = { 'Access-Control-Allow-Origin': '*', };
-  if (ctx.request.method !== 'OPTIONS') {
-    ctx.response.set({...headers});
-    try {
-      return await next();
-    } catch (e) {
-      e.headers = {...e.headers, ...headers};
-      throw e;
-    }
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    'Access-Control-Allow-Origin': true,
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
+
+const router = new Router();
+
+const tickets = new Tickets();
+
+router.get('/tickets', async (ctx) => {
+  const { method } = ctx.request.query;
+  if (method === 'allTickets') {
+    ctx.response.body = tickets.allTickets();
   }
 
-  if (ctx.request.get('Access-Control-Request-Method')) {
-    ctx.response.set({
-      ...headers,
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
-    });
-    if (ctx.request.get('Access-Control-Request-Headers')) {
-      ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Request-Headers'));
-    }
+  if (method === 'ticketById') {
+    const { id } = ctx.request.query;
+    ctx.response.body = tickets.ticketById(id);
+  }
+});
+
+router.post('/tickets', async (ctx) => {
+  ctx.response.body = ctx.request.body;
+  const { method } = ctx.request.query;
+  if (method === 'createTicket') {
+    const { name, description } = ctx.request.body;
+    tickets.createTicket(name, description);
     ctx.response.status = 204;
   }
 });
 
-app.use(koaBody({
-  text: true,
-  urlencoded: true,
-  multipart: true,
-  json: true,
-}));
-
-function tickets() {
-  const arr = [];
-  ticketsFull.forEach((elem) => {
-    arr.push(new Ticket(elem.id, elem.name, elem.status, elem.created));
-  });
-  return arr;
-}
-
-function findTicket(id) {
-  const result = ticketsFull.find((ticket) => ticket.id === id);
-  return result;
-}
-
-app.use(async ctx => {
-  const params = new URLSearchParams(ctx.request.querystring);
-  const obj = { method: params.get('method'), id: params.get('id') };
-  const { method, id } = obj;
-  const { body } = ctx.request;
-
-  console.log('method:', method, 'id:', id, 'ctx', body);
-  switch (method) {
-    case 'allTickets':
-      ctx.response.body = tickets();
-      return;
-    case 'ticketById':
-      if (ctx.request.query.id) {
-        ctx.response.body = findTicket(+id);
-      }
-      return;
-    case 'createTicket':
-      const nextId = ticketsFull.length;
-      ticketsFull.push(new TicketFull(nextId, body.title, body.description, false, new Date().toString().slice(3,21)));
-      ctx.response.body = ticketsFull[nextId];
-      console.log(ticketsFull.length);
-      return;
-    case 'editTicket':
-      const index = body.id;
-      ticketsFull[index].name = body.title;
-      ticketsFull[index].description = body.description;
-      ctx.response.body = ticketsFull[index];
-      return;
-    case 'deleteTicket':
-      const ind = ticketsFull.findIndex((ticket) => +ticket.id === +id);
-      console.log('index', ind);
-      ctx.response.body = 'del';
-      ticketsFull.splice(ind, 1);
-      return;
-    default:
-      ctx.response.status = 404;
-      return;
-  }
-});
-
-app.use(async (ctx) => {
-  console.log('request.querystring:', ctx.request.querystring);
-  console.log('request.body', ctx.request.body);
+router.delete('/tickets/:id', async (ctx) => {
+  const ticketId = Number(ctx.params.id);
+  tickets.deleteTicket(ticketId);
   ctx.response.status = 204;
-
-  console.log(ctx.response);
 });
+
+router.put('/tickets/:id', async (ctx) => {
+  const ticketId = Number(ctx.params.id);
+  const { name, description } = ctx.request.body;
+  if (!name || !description) {
+    tickets.changeStatus(ticketId);
+  } else {
+    tickets.editTicket(ticketId, name, description);
+  }
+  ctx.response.status = 204;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 const port = process.env.PORT || 7070;
-const server = http.createServer(app.callback()).listen(port);
+const server = http.createServer(app.callback());
+server.listen(port, () => console.log('Server started'));
